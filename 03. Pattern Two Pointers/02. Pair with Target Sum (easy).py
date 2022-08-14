cde61130c37ff0e6735d8a573446870adb92d9c1
Ivan Zimin <zimin.ivan.a@yandex.ru>
2022-08-14 03:08:47 +0300

    Pair with Target Sum (easy)

---
+++ 03. Pattern Two Pointers/02. Pair with Target Sum (easy).py
@@ -0,0 +1,21 @@
+# 03. Pattern Two Pointers
+# 02. Pair with Target Sum (easy)
+"""
+Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+
+Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+"""
+def pair_with_targetsum(arr, target_sum):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        if arr[left] + arr[right] == target_sum:
+            return [left, right]
+        elif arr[left] + arr[right] < target_sum:
+            left += 1
+        else:
+            right -= 1
+            
+
+print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+print(pair_with_targetsum([2, 5, 9, 11], 11))
